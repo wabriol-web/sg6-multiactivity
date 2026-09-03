ba5cefea9eb1fba38a8c51d3fe6cd718b841ecde
wabriol-web <wabriol@brc.pshs.edu.ph>
2026-09-03 20:33:31 +0800

    Problem 2: Grade Evaluator
    Added the second problem code in the google classroom; the grade evaluator.

---
+++ Abriol_Willen_simplegradedetector.py
@@ -0,0 +1,13 @@
+score = int(input("Enter your score: "))
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+else:
+    print("Grade: D")
+
+#The program asks the user to input their score to determine their grade.
+#It uses if elif statements to check what category is the score in.
+#Finally, it prints out the corresponding grade based on the score entered by the user.
