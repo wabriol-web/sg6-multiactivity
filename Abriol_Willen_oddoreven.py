96ec2a6b8181ac169473a65ff74f7abb8a43822f
wabriol-web <wabriol@brc.pshs.edu.ph>
2026-09-03 20:25:52 +0800

    Problem 1: Odd or Even
    Added the first problem complete code: the odd or even problem.

---
+++ Abriol_Willen_oddoreven.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Output: Even")
+else:
+    print("Output: Odd")
+
+#The program asks the user for a number and checks if it is even or odd.
+#It checks by using the modulus operator to determine if the number is divisible by 2.
+#If the remainder is 0, it prints "Even", otherwise it prints "Odd".
